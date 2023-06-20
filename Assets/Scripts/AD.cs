ca321c2ad1d4f96e789d9ee2bc35536eba21fed5
reper <arsenpoter7777777@gmail.com>
2023-06-20 15:56:16 +0300

    davka

---
+++ Assets/Scripts/AD.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AD : MonoBehaviour, IUnityAdsInitializationListener
+{
+    public static bool canWatchAD = false;
+
+    private void Start()
+    {
+        canWatchAD = false;
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1);
+        Advertisement.Initialize("5318615", false, this);
+
+        while (true)
+        {
+            yield return new WaitForSeconds(45);
+            if (canWatchAD)
+            {
+                canWatchAD = false;
+                Advertisement.Load("Interstitial_Android", new Interstitial());
+            }
+        }
+    }
+
+    public void ShowAD()
+    {
+        if (canWatchAD)
+        {
+            canWatchAD = false;
+            Advertisement.Load("Rewarded_Android", new Rewarded());
+        }         
+    }
+
+    public void OnInitializationComplete()
+    {
+        Advertisement.Load("Interstitial_Android", new Interstitial());
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        canWatchAD = true;
+    }
+}
+
+public class Interstitial : IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        Advertisement.Show("Interstitial_Android", this);
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        AD.canWatchAD = true;
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        AD.canWatchAD = true;
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        AD.canWatchAD = true;
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        AD.canWatchAD = true;
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        AD.canWatchAD = true;
+    }
+}
+
+public class Rewarded : IUnityAdsLoadListener,  IUnityAdsShowListener
+{
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        Advertisement.Show("Rewarded_Android", this);
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        AD.canWatchAD = true;
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        AD.canWatchAD = true;
+        Money.money += 999;
+        Money.Save();
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        AD.canWatchAD = true;
+        Money.money += 999;
+        Money.Save();
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        AD.canWatchAD = true;
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        AD.canWatchAD = true;
+    }
+}
---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditor;
+
+public class Money : MonoBehaviour
+{
+    public static int income = 1;
+    public static int money;
+    public static int upgradeCost;
+    public static int level = 1;
+
+    [SerializeField] private AudioSource buy, buyError, click;
+
+    public TextMeshProUGUI incomeText;
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI upgradeCostText;
+
+    public Animator cube;
+
+    private void Start()
+    {
+        //PlayerPrefs.DeleteAll();
+        income = PlayerPrefs.GetInt("income", 1);
+        money = PlayerPrefs.GetInt("money", 0);
+        upgradeCost = PlayerPrefs.GetInt("upgradeCost", 10);
+        level = PlayerPrefs.GetInt("level", 1);
+    }
+
+    public void CubeClick()
+    {
+        money += income;
+        cube.Play("cube_click");
+        click.Stop(); click.Play();
+        Save();
+    }
+
+    public void UpgradeIncome()
+    {
+        if(money >= upgradeCost)
+        {
+            buy.Stop(); buy.Play();
+
+            income += level;
+            money -= upgradeCost;
+
+            if (upgradeCost < 100)
+            {
+                upgradeCost += 10; level = 1;
+            }
+            else if (upgradeCost >= 100 && upgradeCost < 1000)
+            {
+                upgradeCost += 100; level = 3;
+            }
+            else if (upgradeCost >= 1000)
+            {
+                upgradeCost += 500; level = 5;
+            }
+        }
+        else
+        {
+            buyError.Stop(); buyError.Play();
+        }
+
+
+        Save();
+    }
+
+    public static void Save()
+    {
+        PlayerPrefs.SetInt("income", income);
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.SetInt("upgradeCost", upgradeCost);
+        PlayerPrefs.SetInt("level", level);
+    }
+
+    void Update()
+    {
+        moneyText.text = money.ToString();
+        incomeText.text = "Income: $" + income.ToString();
+        upgradeCostText.text = "UPGRADE INCOME: $" + upgradeCost.ToString();
+    }
+}
